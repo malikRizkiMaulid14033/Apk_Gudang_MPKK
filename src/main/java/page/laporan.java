1ca1e46c4c9a90a1d5445eb415c845164fad4dc9
AhnafZakhy3 <ahnafzakhy@gmail.com>
2026-06-10 07:47:51 +0700

    re

--- src/main/java/page/laporan.java
+++ src/main/java/page/laporan.java
@@ -168,7 +168,7 @@ Class_Laporan backendLaporan = new Class_Laporan();
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jLabel6)))
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addContainerGap(24, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
